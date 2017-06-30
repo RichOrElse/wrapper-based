2a2b179907460eb74e7c99ec6276576116447bad
Ritchie Paul Buitre <ritchie@richorelse.com>
2017-06-30 17:59:53 +0800

    remove WrapperBased::Decorator and WrapperBased::FORWARDING

--- lib/wrapper_based.rb
+++ lib/wrapper_based.rb
@@ -2,20 +2,6 @@ require "wrapper_based/version"
 require "wrapper_based/cast"
 require "wrapper_based/context"
 require "wrapper_based/context/builder"
-require "wrapper_based/decorator"
 
 module WrapperBased
-  FORWARDING = -> type, *behaviors do
-                Class.new(Decorator) do
-                  using Module.new { refine(type) { prepend(*behaviors.reverse) } }
-
-                  forwarding = behaviors.flat_map(&:public_instance_methods) - public_instance_methods
-
-                  forwarding.uniq.each do |meth|
-                    define_method(meth) do |*args, &block|
-                      __getobj__.send(meth, *args, &block)
-                    end
-                  end
-                end
-              end
 end
--- test/wrapper_based_test.rb
+++ test/wrapper_based_test.rb
@@ -4,37 +4,4 @@ class WrapperBasedTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::WrapperBased::VERSION
   end
-
-  def test_decorator_enscapulation
-    type = Class.new do
-      def accessible
-        :visible
-      end
-
-      def secret
-        :hidden
-      end
-
-      private :secret
-    end
-
-    behavior = Module.new do
-      def accessible2
-        :visible
-      end
-
-      def secret2
-        :hidden
-      end
-
-      private :secret2
-    end
-
-    wrapped = WrapperBased::FORWARDING[type, behavior].new(type.new)
-
-    assert_equal :visible, wrapped.accessible
-    assert_equal :visible, wrapped.accessible2
-    assert_raises(NameError) { wrapped.secret }
-    assert_raises(NameError) { wrapped.secret2 }
-  end
 end
