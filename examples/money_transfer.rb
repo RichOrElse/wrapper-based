0df8eb96ef915fddbda9cb16c1606d65ae7ab594
Ritchie Paul Buitre <ritchie@richorelse.com>
2017-06-30 17:57:37 +0800

    add Funds namespace to money transfer

--- examples/money_transfer.rb
+++ examples/money_transfer.rb
@@ -1,41 +1,45 @@
 LogTransaction = Struct.new(:transaction_type, :amount, :account_number)
-Account = Struct.new(:number, :balance)
-NotEnoughFunds = Class.new(StandardError)
 
-module SourceAccount
-  def decrease_balance_by(amount)
-    fail NotEnoughFunds, "Balance is below amount.", caller if balance < amount
-    self.balance -= amount
-  end
-end
+module Funds
+  Insufficient = Class.new(StandardError)
 
-module DestinationAccount
-  def increase_balance_by(amount)
-    self.balance += amount
-  end
-end
-
-DCI = WrapperBased::DCI.new unless defined? DCI
-
-class TransferMoney < DCI::Context(:from, :to)
-  from.as SourceAccount
-  to.as DestinationAccount
-
-  def withdraw(amount)
-    from.decrease_balance_by(amount)
-    LogTransaction["Withdraw", amount, from.number]
+  module SourceAccount
+    def decrease_balance_by(amount)
+      fail Funds::Insufficient, "Balance is below amount.", caller if balance < amount
+      self.balance -= amount
+    end
   end
 
-  def deposit(amount)
-    to.increase_balance_by(amount)
-    LogTransaction["Deposit", amount, to.number]
+  module DestinationAccount
+    def increase_balance_by(amount)
+      self.balance += amount
+    end
   end
 
-  def call(amount:)
-    accounts = [@from, @to]
-    transaction_logs = [withdraw(amount), deposit(amount)]
-    [:success, { logs: transaction_logs }, accounts]
-  rescue NotEnoughFunds => error
-    [:failure, { message: error.message }, accounts]
+  class TransferMoney < DCI::Context(:from, :to)
+    from.as SourceAccount
+    to.as DestinationAccount
+
+    def withdraw(amount)
+      from.decrease_balance_by(amount)
+      LogTransaction["Withdraw", amount, from.number]
+    end
+
+    def deposit(amount)
+      to.increase_balance_by(amount)
+      LogTransaction["Deposit", amount, to.number]
+    end
+
+    def call(amount:)
+      accounts = [@from, @to]
+      transaction_logs = [withdraw(amount), deposit(amount)]
+      [:success, { logs: transaction_logs }, accounts]
+    rescue NotEnoughFunds => error
+      [:failure, { message: error.message }, accounts]
+    end
+  end
+
+  def self.transfer(**where)
+    TransferMoney[**where]
   end
 end
--- test/money_transfer_test.rb
+++ test/money_transfer_test.rb
@@ -1,29 +1,31 @@
 require 'test_helper'
 require_relative '../examples/money_transfer'
 
-class TransferMoneyTest < MiniTest::Test
+class FundsTransferTest < MiniTest::Test
+  Account = Struct.new(:number, :balance)
+
   def setup
     @account1= Account['A1', 50]
     @account2 = Account['B2', 100]
   end
 
   def test_deposit
-    TransferMoney[to: @account1].deposit(10)
+    Funds.transfer(to: @account1).deposit(10)
     assert_equal 60, @account1.balance
   end
 
   def test_withdraw
-    TransferMoney[from: @account1].withdraw(10)
+    Funds.transfer(from: @account1).withdraw(10)
     assert_equal 40, @account1.balance
   end
 
   def test_withdraw_above_balance
-    error = assert_raises(NotEnoughFunds) { TransferMoney[from: @account1].withdraw(60) }
+    error = assert_raises(Funds::Insufficient) { Funds.transfer(from: @account1).withdraw(60) }
     assert_equal "Balance is below amount.", error.message
   end
 
   def test_call
-    TransferMoney[from: @account1, to: @account2].(amount: 50)
+    Funds.transfer(from: @account1, to: @account2).(amount: 50)
     assert_equal 0, @account1.balance
     assert_equal 150, @account2.balance
   end
