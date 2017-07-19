757bd9c4ae6de204eb41a09f7650fd77bba3642e
Ritchie Paul Buitre <ritchie@richorelse.com>
2017-07-20 02:16:40 +0800

    refactor Dijkstra example code

--- examples/dijkstra.rb
+++ examples/dijkstra.rb
@@ -1,18 +1,10 @@
 require_relative 'dijkstra/data'
 # See https://github.com/RichOrElse/wrapper-based/blob/master/test/dijkstra_test.rb
 
-module CurrentIntersection
-  def neighbors(manhattan:)
-    east_neighbor = manhattan.east_neighbor_of(self)
-    south_neighbor = manhattan.south_neighbor_of(self)
-    [south_neighbor, east_neighbor].compact # excludes nil neighbors
-  end
-end
-
 module DestinationNode
-  def shortest_path(from:, within:)
-    return [self] if equal? from
-    FindShortest[to: self, from: from, city: within].path
+  def shortest_path_from(neighbor, find_shortest)
+    return [self] if equal? neighbor
+    find_shortest.path(from: neighbor)
   end
 end
 
@@ -24,23 +16,28 @@ Map = DCI::Module.new do |mod| using mod
   def distance_of(path)
     path.each_cons(2).inject(0) { |total, (to, from)| total + distance_between(from, to) }
   end
+
+  def neighbors(near:)
+    [south_neighbor_of(near), east_neighbor_of(near)].compact # excludes nil neighbors
+  end
 end
 
-class FindShortest < DCI::Context(from: CurrentIntersection, to: DestinationNode, city: Map)
+class FindShortest < DCI::Context(:from, to: DestinationNode, city: Map)
+  def initialize(city:, from: city.root, to: city.destination) super end
+
   def distance
     city.distance_of path
   end
 
-  def path
-    shortest_path_from_city_neighbors << @from
+  def path(from: @from)
+    shortest_neighbor_path(from) << from
   end
 
   private
 
-  def shortest_path_from_city_neighbors
-    from.
-      neighbors(manhattan: @city).
-      map { |neighbor| to.shortest_path from: neighbor, within: @city }.
-      min_by { |path| city.distance_of path }
+  def shortest_neighbor_path(current)
+    city.neighbors(near: current).
+      map { |neighbor| to.shortest_path_from(neighbor, self) }.
+      min_by { |neighbor_path| city.distance_of neighbor_path }
   end
 end
